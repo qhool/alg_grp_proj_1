468e1809a7532c23ae853ee19442b0e87e028444
Josh Burroughs <josh@qhool.com>
2015-09-10 23:19:24 -0400

    bring dual-pivot sort in line with paper
      * eliminate use of scratch buffers, in preference for swapping
      * bring variable names closer to those in the paper
      * simplify pivot picking

--- lib/Sorts/dual_pivot.py
+++ lib/Sorts/dual_pivot.py
@@ -2,49 +2,42 @@ import random
 import Sorts
 from copy import copy
 
-def dual_pivot(items,start=0,end=None):
-    if end == None:
-        end = len(items) - 1
-    size = end - start + 1
-    # 17 according to http://iaroslavski.narod.ru/quicksort/DualPivotQuicksort.pdf
-    if size < 17:
-        return Sorts.insertion(items,start,end)
-    # the start of slices is 0-based,
-    # but the end of slices is 1-based
-    # this is the sort of thing that makes me not like python
-    # scratch copy of the list
-    a = items[start:end+1]
+# dual-pivot quicksort, as described in
+# http://iaroslavski.narod.ru/quicksort/DualPivotQuicksort.pdf
+def dual_pivot(T,left=0,right=None):
+    if right == None:
+        right = len(T) - 1
+    size =  right - left + 1
+    # the lower limit of 17 is according to the paper
+    if right - left + 1 < 17:
+        Sorts.insertion(T,left,right)
+        return
 
-    # also need scratch space between the pivots
-    mids = [None]*size
-    low_pos = start
-    hi_pos = end
-    mid_pos = 0
-    pivot1idx = random.randrange(size)
-    pivot2idx = pivot1idx
-    while pivot2idx == pivot1idx:
-        pivot2idx = random.randrange(size)
-    if a[pivot1idx] > a[pivot2idx]:
-        tmp = pivot1idx
-        pivot1idx = pivot2idx
-        pivot2idx = tmp
-    pivot1 = a[pivot1idx]
-    pivot2 = a[pivot2idx]
-    for i in range(0,end-start+1):
-        if i == pivot1idx or i == pivot2idx:
-            continue
-        if a[i] <= pivot1:
-            items[low_pos] = a[i]
-            low_pos += 1
-        elif a[i] > pivot2:
-            items[hi_pos] = a[i]
-            hi_pos -= 1
+    pivot1idx = random.randrange(left,right+1)
+    T[left],T[pivot1idx] = T[pivot1idx],T[left]
+    pivot2idx = random.randrange(left+1,right+1)
+    T[right],T[pivot2idx] = T[pivot2idx],T[right]
+    #make sure left pivot is smaller
+    if T[left] > T[right]:
+        T[left],T[right] = T[right],T[left]
+    P1, P2 = T[left], T[right]
+    K = L = left + 1
+    G = right - 1
+    while K <= G:
+        if T[K] < P1:
+            T[L],T[K] = T[K],T[L]
+            L += 1
+            K += 1
+        elif T[K] > P2:
+            T[K],T[G] = T[G],T[K]
+            # we haven't examined the value that was at T[G]
+            # K is not advanced, so we will check it next
+            G -= 1
         else:
-            mids[mid_pos] = a[i]
-            mid_pos += 1
-    items[low_pos] = pivot1
-    items[hi_pos] = pivot2
-    items[low_pos+1:hi_pos] = mids[0:mid_pos]
-    dual_pivot(items,start,low_pos-1)
-    dual_pivot(items,low_pos+1,hi_pos-1)
-    dual_pivot(items,hi_pos+1,end)
+            K += 1
+    #swap pivots into final positions
+    T[left],T[L-1] = T[L-1],T[left]
+    T[right],T[G+1] = T[G+1],T[right]
+    dual_pivot(T,left,L-1)
+    dual_pivot(T,L,G)
+    dual_pivot(T,G+1,right)
